877cbe29798b8fdb511b8c33541553d0aa99ca1d
Brien Hall <brienhall@Briens-MacBook-Pro.local>
2017-08-08 20:57:08 -0500

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAAA

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,34 +1,32 @@
 $(document).ready(function(){
-  
 
-  // $('.modal-trigger').leanModal();
-
-  $('.modal').modal();
+	$('.modal').modal();
 
 
-  $('.timepicker').pickatime({
-    default: 'now', // Set default time: 'now', '1:30AM', '16:30'
-    fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
-    twelvehour: false, // Use AM/PM or 24-hour format
-    donetext: 'OK', // text for done-button
-    cleartext: 'Clear', // text for clear-button
-    canceltext: 'Cancel', // Text for cancel-button
-    autoclose: false, // automatic close timepicker
-    ampmclickable: true, // make AM PM clickable
-    aftershow: function(){} //Function for after opening timepicker
-  });
+	$('.timepicker').pickatime({
+	default: 'now', // Set default time: 'now', '1:30AM', '16:30'
+	fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
+	twelvehour: false, // Use AM/PM or 24-hour format
+	donetext: 'OK', // text for done-button
+	cleartext: 'Clear', // text for clear-button
+	canceltext: 'Cancel', // Text for cancel-button
+	autoclose: false, // automatic close timepicker
+	ampmclickable: true, // make AM PM clickable
+	aftershow: function(){} //Function for after opening timepicker
+	});
+
+	var queryURL = 'http://quandyfactory.com/insult/json';
+
+	$.ajax({
+		url: queryURL,
+		method: "GET",
 
 
-
-
-
-
-
-
-
-
-
-
+	})
+	.done(function(response) {
+		var randomInsult = response.insult;
+		$("#submission-insult").html(randomInsult);
+	})
 
 
 
