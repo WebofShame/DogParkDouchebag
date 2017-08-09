8d695bd17b53309ec0bf7d1023399a020435a118
Brien Hall <brienhall@Briens-MacBook-Pro.local>
2017-08-08 21:26:21 -0500

    Added shit emoji favicon; added css to widen modal and freeze scrolling with modal open; added code for auto date/time, needs to be debugged

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -15,19 +15,56 @@ $(document).ready(function(){
 	aftershow: function(){} //Function for after opening timepicker
 	});
 
-	var queryURL = 'http://quandyfactory.com/insult/json';
+	//adds already randomized Elizibethan insult to small card on right side
+	function insultor() {
+		var queryURL = 'http://quandyfactory.com/insult/json';
 
-	$.ajax({
-		url: queryURL,
-		method: "GET",
-
-
-	})
-	.done(function(response) {
-		var randomInsult = response.insult;
-		$("#submission-insult").html(randomInsult);
-	})
+		$.ajax({
+			url: queryURL,
+			method: "GET",
 
 
+		})
+		.done(function(response) {
+			var randomInsult = response.insult;
+			$("#submission-insult").html(randomInsult);
+		})
+	}
+
+	// bind rover to 5
+     $('#rover').poopRating(5, 5);
+     // here we are listening for when rover takes a dump because we are responsible owners
+     $('#rover').on('pooped', function (event, value) {
+        console.log(value);
+         alert('Your dog pooped ' + value + ' times....');
+     });
+
+     //  Made the comonent so that you can initialize using either direct id selector or by css class
+     $('.scruffy-and-family').poopRating(5, 3);
+
+     $(function(){
+		$('input[type="time"][value="now"]').each(function(){
+			var d = new Date();
+			h = d.getHours();
+			m = d.getMinutes();
+			if(h < 10) h = '0' + h;
+			if(m < 10) m = '0' + m;
+			$(this).attr({
+				'value': h + ':' + m
+			});
+		});
+  	});
+
+	$(function(){
+		$('input[type="text"][value="day"]').each(function(){
+			var today = new Date();
+			var month = today.getMonth();
+			var date = today.getDate();
+			var year = today.getFullYear();
+			$(this).attr({
+				"value": month + "/" + date + "/" + year
+			});
+		});
+	});
 
 });
